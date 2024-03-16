c25977cea58cdd7faefcae8678807beeafc7c3cf
Ridwan4D <ridwanul161@gmail.com>
2024-03-17 01:53:51 +0600

    created part is responsive now

--- src/components/Cooking.jsx
+++ src/components/Cooking.jsx
@@ -7,7 +7,7 @@ const Cooking = ({ sendItem,handlePreparing,reSendCart,time,calories}) => {
     <div className="w-full md:w-2/4 p-2 border-2 border-gray-400 rounded-2xl h-full md:mt-5">
       <Table handlePreparing={handlePreparing} sendItem={sendItem}></Table>
       <CookTable reSendCart={reSendCart}></CookTable>
-      <div className="my-5 px-5 text-2xl font-semibold text-[#282828CC] flex">
+      <div className="my-5 px-2 md:px-5 md:text-2xl font-semibold text-[#282828CC] flex">
         <p>Total Time = {time} minutes</p>
         <p>Total Calories = {calories} calories</p>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,54 @@
 const Navbar = () => {
   return (
     <div>
-      <div className="navbar bg-base-100">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+      <div className="md:navbar flex flex-col md:flex-row items-center bg-base-100">
+        <div className="flex flex-row md:w-8/12">
+          <div className="md:navbar-start">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="#recipes-section">Recipes</a>
+                </li>
+                <li>
+                  <a href="#">About</a>
+                </li>
+                <li>
+                  <a href="#">Search</a>
+                </li>
+              </ul>
             </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-            >
+            <a className="btn btn-ghost text-xl md:text-3xl font-bold">
+              Recipe Calories
+            </a>
+          </div>
+          <div className="md:navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1 text-base text-[#150B2BB3]">
               <li>
                 <a href="#">Home</a>
               </li>
@@ -38,45 +63,15 @@ const Navbar = () => {
               </li>
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl md:text-3xl font-bold">
-            Recipe Calories
-          </a>
         </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1 text-base text-[#150B2BB3]">
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#recipes-section">Recipes</a>
-            </li>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Search</a>
-            </li>
-          </ul>
-        </div>
-        <div className="md:navbar-end space-x-2 md:space-x-3 absolute top-12 md:relative md:top-0">
-          <label className="input input-bordered flex items-center gap-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              className="w-4 h-4 opacity-70"
-            >
-              <path
-                fillRule="evenodd"
-                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                clipRule="evenodd"
-              />
-            </svg>
+        <div className="md:navbar-end md:space-x-3 flex items-center justify-center md:w-4/12">
+          <label className="input input-bordered flex items-center gap-2 w-8/12 h-[40px] md:w-2/4 md:h-[50px] mr-2">
+            <i className="fa-solid fa-magnifying-glass"></i>
             <input type="text" className="grow" placeholder="Search" />
           </label>
-          <a className="btn h-12 w-12 rounded-full btn-bg">
-            <i className="fa-solid fa-user-plus btn-text border-2 border-[#150B2B] rounded-full p-1"></i>
-          </a>
+          <button className="btn rounded-full btn-bg">
+            <i className="fa-solid fa-user-plus btn-text md:border-2 border-[#150B2B] rounded-full md:p-1"></i>
+          </button>
         </div>
       </div>
     </div>
